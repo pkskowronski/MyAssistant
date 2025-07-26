1be707aeaa5d681cb2751226d469e3d7728d7d8a
Paweł Skowroński <psk@trackman.com>
2025-07-26 14:26:34 +0200

    Installed Todoist nuget, added todoist service

---
+++ Application/Services/TodoistService.cs
@@ -0,0 +1,23 @@
+﻿using Application.Abstractions;
+using Microsoft.Extensions.Configuration;
+using Todoist.Net;
+
+namespace Application.Services
+{
+    internal class TodoistService : ITodoistService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ITodoistClient _todoistClient;
+        public TodoistService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _todoistClient = new TodoistClient(_configuration["TodoistApiKey"]);
+        }
+
+        public async Task<string> GetTodoistTasksAsync()
+        {
+            var tasks = await _todoistClient.Projects.GetAsync();
+            return string.Join(Environment.NewLine, tasks.Select(t => $"{t.Content} (ID: {t.Id})"));
+        }
+    }
+}
